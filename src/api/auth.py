ce2e5cd7348f5f7bf9d3054ebc14d0fd203d7700
Spartan0nix <l.humblot74@gmail.com>
2022-01-29 13:32:41 +0100

    Update auth method arguments

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,21 +1,20 @@
 import requests
-import json
 from src.api.utils import headers
 
 
-def auth(env_json: json) -> str:
+def auth(url: str, user: str, password: str) -> str:
     data = {
         "jsonrpc": "2.0",
         "method": "user.login",
         "params": {
-            "user": env_json['user'],
-            "password": env_json['password']
+            "user": user,
+            "password": password
         },
         "id": 1
     }
 
     try:
-        response = requests.post(env_json['url'], headers=headers, json=data, verify=False)
+        response = requests.post(url, headers=headers, json=data, verify=False)
         json_response = response.json()
         if "error" in json_response:
             print("Erreur lors de l'authentification au server Zabbix. Raison : {}".format(json_response['error']))
