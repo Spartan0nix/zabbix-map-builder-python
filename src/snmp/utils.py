5ad9724b38dac94ac2508db6cdc0f904367b6ad9
Spartan0nix <l.humblot74@gmail.com>
2022-02-06 22:17:34 +0100

    Update return type

--- src/snmp/utils.py
+++ src/snmp/utils.py
@@ -2,7 +2,7 @@ import re
 import pysnmp
 
 
-def extract_indexes_and_ip(interfaces: list) -> list:
+def extract_indexes_and_ip(interfaces: list) -> list[dict[str, str]]:
     indexes = []
     for interface in interfaces:
         router_interface = {}
@@ -16,8 +16,8 @@ def extract_indexes_and_ip(interfaces: list) -> list:
             remote_ip_tuple = oid[1].asNumbers()
             remote_ip = '.'.join(str(x) for x in remote_ip_tuple)
 
-            router_interface['index'] = index
-            router_interface['remote_ip'] = remote_ip
+            router_interface['index'] = str(index)
+            router_interface['remote_ip'] = str(remote_ip)
             indexes.append(router_interface)
 
     return indexes
