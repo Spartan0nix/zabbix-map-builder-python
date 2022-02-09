a91e98219a18f7496b2f344b25baad875cea2ccd
Spartan0nix <l.humblot74@gmail.com>
2022-02-09 20:47:51 +0100

    Separate map building from the function adding elements to the map

--- main.py
+++ main.py
@@ -37,7 +37,10 @@ import src.snmp.utils as snmp_utils
 #     }
 # ]
 
-def build_router_connections(router_ip: str) -> list[dict[str, str, str, str]]:
+def get_router_connections(router_ip: str) -> list[dict[str, str, str, str]]:
+    load_dotenv()
+    snmp_community = os.getenv("SNMP_COMMUNITY")
+
     connections = {'routers': []}
 
     # Get local_hostname
@@ -79,50 +82,28 @@ def build_router_connections(router_ip: str) -> list[dict[str, str, str, str]]:
     return connections["routers"]
 
 
-def build_map(connections: list):
-    load_dotenv()
-
-    ZABBIX_URL = os.getenv("ZABBIX_URL")
-    MAP_NAME = os.getenv("ZABBIX_MAP_NAME")
-    ZABBIX_TOKEN = api_auth.auth(ZABBIX_URL, os.getenv("ZABBIX_USER"), os.getenv("ZABBIX_USER_PASSWORD"))
+def build_map(url: str, token: str, map: dict, connections: list):
     map_hosts = []
-
-    # Get the map by name
-    zabbix_map = api_map.get_by_name(ZABBIX_URL, ZABBIX_TOKEN, MAP_NAME)
-
-    # Check if the map exist
-    if not zabbix_map:
-        map_id = api_map.create(ZABBIX_URL, ZABBIX_TOKEN, MAP_NAME)
-    else:
-        map_id = zabbix_map["sysmapid"]
-
-    # Retrieve the map
-    zabbix_map = api_map.get_by_id(ZABBIX_URL, ZABBIX_TOKEN, map_id)
-
-    # Flush the map from any host
-    zabbix_map['selements'].clear()
-    zabbix_map['links'].clear()
-    api_map.update(ZABBIX_URL, ZABBIX_TOKEN, zabbix_map)
 
     for connection in connections:
         # Get host's id
-        local_host_id = api_host.get_host_id(ZABBIX_URL, connection["local_hostname"], ZABBIX_TOKEN)
-        remote_host_id = api_host.get_host_id(ZABBIX_URL, connection["remote_hostname"], ZABBIX_TOKEN)
+        local_host_id = api_host.get_host_id(url, connection["local_hostname"], token)
+        remote_host_id = api_host.get_host_id(url, connection["remote_hostname"], token)
         # Get trigger's id
-        local_trigger_id = api_trigger.get_id(ZABBIX_URL, ZABBIX_TOKEN, local_host_id, connection["local_interface"])
-        remote_trigger_id = api_trigger.get_id(ZABBIX_URL, ZABBIX_TOKEN, remote_host_id, connection["remote_interface"])
+        local_trigger_id = api_trigger.get_id(url, token, local_host_id, connection["local_interface"])
+        remote_trigger_id = api_trigger.get_id(url, token, remote_host_id, connection["remote_interface"])
 
         # Build and add the hosts to the current map if they are not present
         if local_host_id not in map_hosts:
             local_host = api_host.create(connection["local_hostname"], local_host_id)
-            zabbix_map['selements'].append(local_host)
+            map['selements'].append(local_host)
 
             # Update the tracking hosts list
             map_hosts.append(local_host_id)
 
         if remote_host_id not in map_hosts:
             remote_host = api_host.create(connection["remote_hostname"], remote_host_id)
-            zabbix_map['selements'].append(remote_host)
+            map['selements'].append(remote_host)
 
             # Update the tracking hosts list
             map_hosts.append(remote_host_id)
@@ -136,9 +117,9 @@ def build_map(connections: list):
                                remote_trigger_id)
 
         # Add the link to the current map
-        zabbix_map['links'].append(link)
+        map['links'].append(link)
 
-        api_map.update(ZABBIX_URL, ZABBIX_TOKEN, zabbix_map)
+        api_map.update(url, token, map)
 
 
 if __name__ == '__main__':
@@ -147,13 +128,34 @@ if __name__ == '__main__':
     R1 = os.getenv("R1")
     R2 = os.getenv("R2")
     R3 = os.getenv("R3")
-    snmp_community = os.getenv("SNMP_COMMUNITY")
 
-    routers_info = build_router_connections(R3)
+    ZABBIX_URL = os.getenv("ZABBIX_URL")
+    MAP_NAME = os.getenv("ZABBIX_MAP_NAME")
+    ZABBIX_TOKEN = api_auth.auth(ZABBIX_URL, os.getenv("ZABBIX_USER"), os.getenv("ZABBIX_USER_PASSWORD"))
+
+    routers_info = get_router_connections(R2)
 
     print("------------------------------------------------------------")
     print("-    The following entries will be use to build the map   -")
     print("------------------------------------------------------------")
     [print(x) for x in routers_info]
+
+    # Get the map by name
+    zabbix_map = api_map.get_by_name(ZABBIX_URL, ZABBIX_TOKEN, MAP_NAME)
+
+    # Check if the map exist
+    if not zabbix_map:
+        map_id = api_map.create(ZABBIX_URL, ZABBIX_TOKEN, MAP_NAME)
+    else:
+        map_id = zabbix_map["sysmapid"]
+
+    # Get back the complete map description
+    zabbix_map = api_map.get_by_id(ZABBIX_URL, ZABBIX_TOKEN, map_id)
+
+    # Flush the map from any host
+    zabbix_map['selements'].clear()
+    zabbix_map['links'].clear()
+    api_map.update(ZABBIX_URL, ZABBIX_TOKEN, zabbix_map)
+
     # Populate the zabbix map
-    build_map(routers_info)
+    build_map(ZABBIX_URL, ZABBIX_TOKEN, zabbix_map, routers_info)
